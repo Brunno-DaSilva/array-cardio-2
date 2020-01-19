a7312dd1c92bd2df8ae6c5749278695e7a39be9c
Bruno DaSilva <bananaatomica@Brunos-MacBook-Air.local>
2020-01-18 20:00:46 -0600

    Added syntex highlighted with prim and style to codes

--- app.js
+++ app.js
@@ -1,55 +1,51 @@
 console.log("Ciao mundo");
 
- // ## Array Cardio Day 2
+// ## Array Cardio Day 2
 
- const people = [
-  { name: 'Wes', year: 1988 },
-  { name: 'Kait', year: 1986 },
-  { name: 'Irv', year: 1970 },
-  { name: 'Lux', year: 2015 }
+const people = [
+  { name: "Bruno", year: 1989 },
+  { name: "Liz", year: 1991 },
+  { name: "Ian", year: 2017 },
+  { name: "BabyIncognito", year: 2020 }
 ];
 
 const comments = [
-  { text: 'Love this!', id: 523423 },
-  { text: 'Super good', id: 823423 },
-  { text: 'You are the best', id: 2039842 },
-  { text: 'Ramen is my fav food ever', id: 123523 },
-  { text: 'Nice Nice Nice!', id: 542328 }
+  { text: "Let's Code an hour a day", id: 33325 },
+  { text: "I start my day with a dev Article", id: 552658 },
+  { text: "Two JavaScript Challenges", id: 10205688 },
+  { text: "Work on something meaningful", id: 23566 },
+  { text: "Relax, enjoy family, meditate", id: 542328 }
 ];
 
-
-
 /*******************************
-*           some()  
-*******************************/
-// Array.prototype.some() 
+ *           some()
+ *******************************/
+// Array.prototype.some()
 // is at least one person 19 or older?
 
-const isAdult =  people.some((person) => {
-  const currentYear = (new Date()).getFullYear(); 
-  return currentYear - person.year >=19; 
-})
-
+const isAdult = people.some(person => {
+  const currentYear = new Date().getFullYear();
+  return currentYear - person.year >= 19;
+});
 
 console.table(isAdult);
 
 /*******************************
-*             every()  
-*******************************/
+ *         every()
+ *******************************/
 
-// Array.prototype.every() 
+// Array.prototype.every()
 // is everyone 19 or older?
 
-const areTheyAdults =  people.every((person) => {
-  const currentYear = (new Date()).getFullYear(); 
-  return currentYear - person.year <=19; 
-})
+const areTheyAdults = people.every(person => {
+  const currentYear = new Date().getFullYear();
+  return currentYear - person.year <= 19;
+});
 console.log(areTheyAdults);
 
-
 /*******************************
-*           find()
-*******************************/
+ *           find()
+ *******************************/
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
@@ -58,19 +54,15 @@ const comment = comments.find(comment => comment.id === 823423);
 
 console.log(comment);
 
-
 /*******************************
-*           findIndex()
-*******************************/
+ *           findIndex()
+ *******************************/
 // Find the comment with this ID
 // delete the comment with the ID of 823423
 
-    const index = comments.findIndex(comment => comment.id === 823423);
-    console.log(index);
+const index = comments.findIndex(comment => comment.id === 823423);
+console.log(index);
 
-    // comments.splice(index, 1);
+// comments.splice(index, 1);
 
-    const newComments = [
-      ...comments.slice(0, index),
-      ...comments.slice(index + 1)
-    ];
+const newComments = [...comments.slice(0, index), ...comments.slice(index + 1)];
